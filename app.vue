e03bb44b06af49ec18193bfc263f28c9ea748ab8
Gabriel Gomes <gavitogomes55@gmail.com>
2024-05-02 21:30:42 -0300

    feat: first commit

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="background">
+        <NuxtPage />
+    </div>
+</template>
+
+<style>
+.background {
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    background-image: url('/img/background.png');
+    background-position: center bottom -50px;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="flex justify-center">
+        <img src="/img/logo.png" class="mt-16" />
+    </div>
+</template>
+
+<style></style>
